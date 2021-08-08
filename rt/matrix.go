f67f38cc625db8d1e444bfabaf1e7925738f625a
Sherman Rose <sherman@shermanrose.uk>
2021-08-08 15:49:00 +0100

    First matrix test passed

--- rt/matrix.go
+++ rt/matrix.go
@@ -1,16 +1,108 @@
 package rt
 
-type Matrix struct {
-	Size int
-	Data [][]float64
+import "fmt"
+
+type Matrix interface {
+	Dims() (r, c int)
+	At(row, col int) float64
+	Set(data []float64)
+	Data() []float64
+	Equal(m Matrix)
 }
 
-func NewMatrix(size int) *Matrix {
-	m := new(Matrix)
-	m.Data = make([][]float64, size)
+func MatrixEqual(a Matrix, b Matrix) bool {
+	ar, ac := a.Dims()
+	br, bc := b.Dims()
 
-	for i := range m.Data {
-		m.Data[i] = make([]float64, size)
+	if ar != br || ac != bc {
+		panic("Cannot compare matricies with different dimensions")
 	}
+
+	ad := a.Data()
+	bd := b.Data()
+
+	for i := 0; i < len(ad); i++ {
+		if !Equal(ad[i], bd[i]) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func MatrixCheckBounds(row, col int, m Matrix) {
+
+	mr, mc := m.Dims()
+	if row < 0 || col < 0 || row >= mr || col >= mc {
+		panic(fmt.Sprintf("Invalid access to matrix data: Tried %d:%d, on %d:%d", row, col, mr, mc))
+	}
+}
+
+func MatrixGet(r, c int, m Matrix) float64 {
+	MatrixCheckBounds(r, c, m)
+	mr, _ := m.Dims()
+	return m.Data()[c+(r*mr)]
+}
+
+func MatrixCheckSet(v []float64, m Matrix) {
+
+	c, r := m.Dims()
+	l := c * r
+
+	if v == nil {
+		panic("mat data nil")
+	}
+
+	if len(v) != l || cap(v) != l {
+		panic("mat data wrong shape")
+	}
+}
+
+type Matrix2 struct {
+	Matrix
+	values []float64
+}
+
+type Matrix3 struct {
+	Matrix
+	values []float64
+}
+
+type Matrix4 struct {
+	Matrix
+	values []float64
+}
+
+func (m *Matrix2) Data() []float64 { return m.values }
+func (m *Matrix3) Data() []float64 { return m.values }
+func (m *Matrix4) Data() []float64 { return m.values }
+
+func (m *Matrix2) Dims() (r, c int) { return 2, 2 }
+func (m *Matrix3) Dims() (r, c int) { return 3, 3 }
+func (m *Matrix4) Dims() (r, c int) { return 4, 4 }
+
+func (m *Matrix2) At(row, col int) float64 { return MatrixGet(row, col, m) }
+func (m *Matrix3) At(row, col int) float64 { return MatrixGet(row, col, m) }
+func (m *Matrix4) At(row, col int) float64 { return MatrixGet(row, col, m) }
+
+func (m *Matrix2) Set(values []float64) { MatrixCheckSet(values, m); m.values = values }
+func (m *Matrix3) Set(values []float64) { MatrixCheckSet(values, m); m.values = values }
+func (m *Matrix4) Set(values []float64) { MatrixCheckSet(values, m); m.values = values }
+
+func NewMatrix2(values []float64) *Matrix2 {
+	m := new(Matrix2)
+	m.Set(values)
+	return m
+}
+
+func NewMatrix3(values []float64) *Matrix3 {
+	m := new(Matrix3)
+	m.Set(values)
+	return m
+}
+
+func NewMatrix4(values []float64) *Matrix4 {
+	m := new(Matrix4)
+	m.Set(values)
 	return m
 }
--- rt/matrix_test.go
+++ rt/matrix_test.go
@@ -1,6 +1,10 @@
 package rt
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 // Scenario: Constructing and inspecting a 4x4 matrix
 // 	Given the following 4x4 matrix M:
@@ -17,5 +21,20 @@ import "testing"
 // 		And M[3,2] = 15.5
 
 func TestMatrix(t *testing.T) {
+
+	m4 := NewMatrix4([]float64{
+		1, 2, 3, 4,
+		5.5, 6.5, 7.5, 8.5,
+		9, 10, 11, 12,
+		13.5, 14.5, 15.5, 16.5,
+	})
+
+	assert.True(t, Equal(m4.At(0, 0), 1))
+	assert.True(t, Equal(m4.At(0, 3), 4))
+	assert.True(t, Equal(m4.At(1, 0), 5.5))
+	assert.True(t, Equal(m4.At(1, 2), 7.5))
+	assert.True(t, Equal(m4.At(2, 2), 11))
+	assert.True(t, Equal(m4.At(3, 0), 13.5))
+	assert.True(t, Equal(m4.At(3, 2), 15.5))
 
 }
